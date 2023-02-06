821cb0b8658723384998affad0e283c4dbe6f9ac
sahilnarwal2111 <500097751@stu.upes.ac.in>
2023-02-06 21:57:42 +0530

    First Commit

---
+++ Maven.Jenkins/src/main/java/MyPkg/FirstClass.java
@@ -0,0 +1,15 @@
+package MyPkg;
+
+public class FirstClass {
+	
+	public void Display() {
+		System.out.println("Hello Wordl!");
+	}
+	
+	public static void main(String[] args) {
+		FirstClass obj = new FirstClass();
+		obj.Display();
+
+	}
+
+}
