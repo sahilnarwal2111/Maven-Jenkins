d00bbaa4cd6f77c8a3e741f59ff2594dcc8de6db
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2023-03-25 12:27:36 +0530

    Update FirstClass.java

--- Maven.Jenkins/src/main/java/MyPkg/FirstClass.java
+++ Maven.Jenkins/src/main/java/MyPkg/FirstClass.java
@@ -2,13 +2,49 @@ package MyPkg;
 
 public class FirstClass {
 	
-	public void Display() {
-		System.out.println("Hello Wordl!");
-	}
+// 	public void Display() {
+// 		System.out.println("Hello Wordl!");
+// 	}
 	
 	public static void main(String[] args) {
-		FirstClass obj = new FirstClass();
-		obj.Display();
+		double num1, num2, result;
+	        char operator;
+
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.println("Enter first number: ");
+	        num1 = input.nextDouble();
+
+	        System.out.println("Enter second number: ");
+	        num2 = input.nextDouble();
+
+	        System.out.println("Enter operator (+, -, *, /): ");
+	        operator = input.next().charAt(0);
+
+	        switch(operator) {
+	            case '+':
+	                result = num1 + num2;
+	                break;
+
+	            case '-':
+	                result = num1 - num2;
+	                break;
+
+	            case '*':
+	                result = num1 * num2;
+	                break;
+
+	            case '/':
+	                result = num1 / num2;
+	                break;
+
+	            default:
+	                System.out.println("Invalid operator!");
+	                return;
+	        }
+
+	        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+
 
 	}
 
